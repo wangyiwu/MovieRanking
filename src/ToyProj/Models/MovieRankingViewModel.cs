4d1956dab6e630664e00bd7f09e787c686f6d4e2
Phung Xuan Vuong <vuongdingo@gmail.com>
2024-06-18 15:53:41 +0700

    filter

--- src/ToyProj/Models/MovieRankingViewModel.cs
+++ src/ToyProj/Models/MovieRankingViewModel.cs
@@ -4,12 +4,12 @@ namespace ToyProj.Models
 {
     public class MovieRankingViewModel
     {
-        public int? Skip { get; set; }
-        public bool? Top5 { get; set; }
-        public bool? New {  get; set; }
-        public int? Count { get; set; } 
-        public int? Year { get; set; }
-        public bool? All { get; set; }
+        public int Skip { get; set; }
+        public bool Top5 { get; set; } = false;
+        public bool New { get; set; } = false;
+        public int Count { get; set; } 
+        public int Year { get; set; }
+        public bool All { get; set; } = false;
         public string OrderBy { get; set; }
         public string GenreName { get; set; }
         public List<MovieRankingData> Items { get; set; }
